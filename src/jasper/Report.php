abe4956c0bfb64c0305410ed214558bf40a59d9b
Erick Dylan Navarrete de la Vega <ericknavarrete55@gmail.com>
2023-11-06 20:56:22 -0600

    :sparkles: Modifico .jar para admitir json en la data

--- src/jasper/Report.php
+++ src/jasper/Report.php
@@ -5,15 +5,15 @@ namespace Open2code\Pdf\jasper;
 class Report
 {
     private $jarPath = '/jar/JasperReportGenerator.jar';
-    public $data;
+    public $jsonPath;
     public $parameters;
     public $output;
     public $jasperPath;
     public $documentType;
 
-    public function __construct($data, $parameters, $jasperPath, $documentType, $output)
+    public function __construct($jsonPath, $parameters, $jasperPath, $output, $documentType = "pdf")
     {
-        $this->data = $data;
+        $this->jsonPath = $jsonPath;
         $this->parameters = $parameters;
         $this->jasperPath = $jasperPath;
         $this->output = $output;
@@ -25,16 +25,14 @@ class Report
      */
     public function generateReport(): array
     {
-        $this->data = json_encode($this->data, JSON_UNESCAPED_UNICODE | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS);
-
         $this->jarPath = __DIR__.$this->jarPath;
         $this->parameters = $this->toString($this->parameters);
 
-        $arguments = "$this->jasperPath \"".addslashes($this->data)
-            ."\" --output=$this->output --parameters=\"$this->parameters\" --format=" . $this->documentType;
+        $arguments = "$this->jasperPath $this->jsonPath --output=$this->output --parameters=\"$this->parameters\" --format=" . $this->documentType;
 
         $command = "java -Dfile.encoding=UTF-8 -jar $this->jarPath $arguments";
         $exec = exec($command, $execO, $execR);
+
 
         if (is_numeric(strpos(strtolower($exec), 'done'))) {
             return [
